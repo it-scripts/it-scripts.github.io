2d6815dc9426da5227810368e40b06bb3952dfcc
Jon <unseldjonas0@gmail.com>
2024-09-14 21:33:37 +0200

    Fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -12,7 +12,7 @@ const FourOhFour: React.FC = () => {
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-neutral-900">
-      <img src="../public/it-logo.png" width="64" />
+      <img src="/it-logo.png" width="64" />
       <p className="text-4xl text-white">404</p>
       <p className="text-lg text-white">Redirecting...</p>
     </div>
