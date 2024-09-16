04697567d011bd5df8dc2bc3ffbcc1f389c5a801
Jon <unseldjonas0@gmail.com>
2024-09-16 21:32:26 +0200

    Add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://docs.it-scripts.com/',
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+    outDir: './out',
+  };
